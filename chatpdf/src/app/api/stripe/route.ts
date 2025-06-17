cd0d686fc58ebdb7b44d298fb6d5f64ee2ef6132
Aditya Pratap Singh <aditya.singh@strideone.in>
2025-06-18 00:18:04 +0530

    stripe

---
+++ chatpdf/src/app/api/stripe/route.ts
@@ -0,0 +1,51 @@
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import {eq} from "drizzle-orm";
+import { db } from "../../../lib/db";
+import { userSubscription } from "../../../lib/db/schema";
+import { stripe } from "../../../lib/stripe";
+
+const return_url = process.env.NEXT_PUBLIC_URL + '/';
+export async function GET() {
+    try {
+        const {userId} = await auth();
+        const user = await currentUser();
+
+        if(!userId){
+            return new NextResponse("Unauthorized", {status: 401})
+        }
+
+        const _userSubscriptions = await db.select().from(userSubscription).where(eq(userSubscription.userId,userId))
+        if(_userSubscriptions[0] && _userSubscriptions[0].stripeCustomerId){
+            //trying to cancle at the billing portal 
+            const stripeSession =await stripe.billingPortal.sessions.create({
+                customer:_userSubscriptions[0].stripeCustomerId,
+                return_url
+            })
+            return NextResponse.json({url:stripeSession.url})
+        }
+        const stripeSession =await stripe.checkout.sessions.create({
+            success_url: return_url,
+            cancel_url: return_url,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: "auto",
+            customer_email: user?.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price: process.env.STRIPE_PRICE_ID!,
+                    quantity: 1,
+                },
+            ],
+            metadata: {
+                userId,
+            },
+        })
+        // user first time to subscribe
+
+    } catch (error) {
+        
+    }
+   
+}
